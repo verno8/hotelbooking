bcfa5be6eca771d7d7e47acca82bca7d721508d9
Lee Jaekyoung <verno8@naver.com>
2024-09-23 02:40:19 +0000

    commit

--- booking/src/main/java/hotelbooking/infra/BookingController.java
+++ booking/src/main/java/hotelbooking/infra/BookingController.java
@@ -9,15 +9,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 //<<< Clean Arch / Inbound Adaptor
 
 @RestController
-// @RequestMapping(value="/bookings")
+@RequestMapping(value="/bookings")
 @Transactional
 public class BookingController {
 
     @Autowired
     BookingRepository bookingRepository;
+
+    // POST /bookings로 들어오는 요청 처리
+    @PostMapping
+    public ResponseEntity<Booking> createBooking(@RequestBody Booking bookingRequest) {
+        // 저장할 엔티티 생성
+        Booking booking = new Booking();
+        booking.setUserId(bookingRequest.getUserId());
+        booking.setBookId(bookingRequest.getBookId());
+        booking.setHotelId(bookingRequest.getHotelId());
+        booking.setHotelName(bookingRequest.getHotelName());
+        booking.setBookDt(bookingRequest.getBookDt());
+        booking.setBookStatus(bookingRequest.getBookStatus());
+
+        // 데이터베이스에 저장
+        Booking savedBooking = bookingRepository.save(booking);
+
+        // 생성된 엔티티를 응답으로 반환
+        return ResponseEntity.ok(savedBooking);
+    }
 }
 //>>> Clean Arch / Inbound Adaptor
